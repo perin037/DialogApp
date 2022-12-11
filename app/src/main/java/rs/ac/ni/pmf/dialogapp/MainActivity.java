1b6d525b9e6fa5339712f0a67601c373f4ef2da7
perin037 <stefan.radovanovic@pmf.edu.rs>
2022-12-12 00:06:18 +0100

    Add menues

--- app/src/main/java/rs/ac/ni/pmf/dialogapp/MainActivity.java
+++ app/src/main/java/rs/ac/ni/pmf/dialogapp/MainActivity.java
@@ -1,12 +1,17 @@
 package rs.ac.ni.pmf.dialogapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TimePicker;
 import android.widget.Toast;
@@ -17,6 +22,29 @@ public class MainActivity extends AppCompatActivity implements MyDialog.MyDialog
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button showDialogButton = findViewById(R.id.dlgbutton);
+        showDialogButton.setOnClickListener(this::showListDialog);
+
+        registerForContextMenu(showDialogButton);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.button_context_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.context_menu_button_disable:
+                Toast.makeText(this, "Ugasio sam dugme", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                return true;
+        }
+        return super.onContextItemSelected(item);
     }
 
     public void showDialog(View view) {
@@ -58,5 +86,25 @@ public class MainActivity extends AppCompatActivity implements MyDialog.MyDialog
             }
         }, 2022, 11, 11);
         mydialog.show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+            getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.new_game:
+                Toast.makeText(this, "Selected new game", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.quit_game:
+                Toast.makeText(this, "QUIT GAME", Toast.LENGTH_SHORT).show(); break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 }
